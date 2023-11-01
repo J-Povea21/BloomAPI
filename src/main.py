fbe8c945f181a1302a59b34186ba30e6e635e315
Amdres <japovea@uninorte.edu.co>
2023-10-31 21:28:16 -0500

    Cors is working

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
 # This is the main file of the application. Here we manage all the routes!
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from bloom_filter import BloomFilter
 
+# API config
 app = FastAPI()
+origins = ['http://localhost:3000']
 
+app.add_middleware(CORSMiddleware,
+                   allow_origins = origins,
+                   allow_credentials = True,
+                   allow_methods = ['*'],
+                   allow_headers = ['*']
+                   )
+
+
+# Bloom filter object
 filter = BloomFilter(50, 0.05)
 
 
@@ -13,14 +25,14 @@ async def root():
     return {'status': True, 'message': 'API running...'}
 
 
-@app.post("/bloom/add/{username}")
+@app.get("/bloom/add/{username}")
 async def add(username: str):
     filter.add(username)
     return {'status': True, 'message': 'Username added!'}
 
 
-@app.post("/bloom/check/{username}")
+@app.get("/bloom/check/{username}")
 async def check(username: str):
     usernameExists = filter.check_existence(item=username)
-    message = 'The username is already taken' if usernameExists else 'The username is available'
+    message = 'The username is already taken (probably)' if usernameExists else 'The username is available'
     return {'status': usernameExists, 'message': message}
